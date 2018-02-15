9c03d0317f41d5829d90cdc6380aefbee902a2c9
Jay R Bolton <jayrbolton@gmail.com>
2018-02-15 08:44:10 -0800

    Add Vagrant and Docker config files; WIP

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -2,86 +2,43 @@
 
 # Script to set up a Django project on Vagrant.
 
-cat >> /etc/apt/sources.list <<EOT
-deb http://www.rabbitmq.com/debian/ testing main
-EOT
-
-wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-apt-key add rabbitmq-signing-key-public.asc
-
-
 DATABASE_USERNAME="revolv"
 DATABASE_PASSWORD="revolv"
 DATABASE_NAME="revolv_db"
-
 PGSQL_VERSION=9.3
 
 # Need to fix locale so that Postgres creates databases in UTF-8
 locale-gen en_GB.UTF-8
 dpkg-reconfigure locales
 
-export LANGUAGE=en_GB.UTF-8
-export LANG=en_GB.UTF-8
-export LC_ALL=en_GB.UTF-8
-
-# Install essential packages from Apt
 apt-get update -y
-# Python dev packages
-apt-get install -y build-essential python python-dev rabbitmq-server
-# python-setuptools being installed manually
-wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | python
-# Dependencies for image processing with Pillow (drop-in replacement for PIL)
-# supporting: jpeg, tiff, png, freetype, littlecms
-# (pip install pillow to get pillow itself, it is not in requirements.txt)
-apt-get install -y libjpeg-dev libtiff-dev zlib1g-dev libfreetype6-dev liblcms2-dev
-# Git (we'd rather avoid people keeping credentials for git commits in the repo, but sometimes we need it for pip requirements that aren't in PyPI)
-apt-get install -y git
 
 # Postgresql
-if ! command -v psql; then
-    apt-get install -y postgresql-$PGSQL_VERSION libpq-dev
-    cp /vagrant/vagrant/pg_hba.conf /etc/postgresql/$PGSQL_VERSION/main/
-    /etc/init.d/postgresql reload
-fi
+apt-get install -y postgresql-$PGSQL_VERSION libpq-dev
+cp /vagrant/vagrant/pg_hba.conf /etc/postgresql/$PGSQL_VERSION/main/
+service postgresql restart
 
-# virtualenv global setup
-if ! command -v pip; then
-    easy_install -U pip
-fi
-if [[ ! -f /usr/local/bin/virtualenv ]]; then
-    pip install virtualenv virtualenvwrapper stevedore virtualenv-clone
-fi
+# Python
+apt-get install -y build-essential python python-dev python-setuptools
+easy_install -U pip
+pip install -U virtualenv
+virtualenv revolv -p `which python2.7`
+source revolv/bin/activate
+pip install -r requirements/dev.txt
 
-
-# Node.js, CoffeeScript and LESS
-if ! command -v npm; then
-    sudo apt-get --purge remove node
-    sudo apt-get autoremove
-    sudo apt-get install -y nodejs-legacy npm
-fi
-
-sudo apt-get install rubygems-integration
-sudo gem install sass
+# Node.js and npm
+apt-get --purge remove node
+apt-get autoremove
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+apt-get install -y nodejs
 
 sudo -u postgres psql -c "create database $DATABASE_NAME;"
 sudo -u postgres psql -c "create user $DATABASE_USERNAME with password '$DATABASE_PASSWORD';"
 sudo -u postgres psql -c "grant all privileges on database $DATABASE_NAME to $DATABASE_USERNAME;"
 sudo -u postgres psql -c "alter user $DATABASE_USERNAME createdb;"
 
-virtualenv venv
-source venv/bin/activate
-pip install -r /vagrant/requirements.txt
-pre-commit install
-
-sudo npm install -g grunt-cli
-cd /vagrant && npm install
-
-sudo npm install -g bower
-cd /vagrant && bower install --allow-root
-cd /vagrant && grunt sass
-
-# RabbitMQ
-sudo service rabbitmq-server restart
-sudo rabbitmqctl add_vhost revolv
-sudo rabbitmqctl add_user revolv revolv
-sudo rabbitmqctl set_permissions -p revolv revolv ".*" ".*" ".*"
+cd /vagrant
+npm install
+cp revolv/settings/local.example.py revolv/settings/local.py
+echo "DJANGO_SETTINGS_MODULE=revolv.settings.local" > .env
+python manage.py migrate
